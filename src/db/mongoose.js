4f20ac9f4829e7b2ea8477b95ebb253478ff3fc8
arryan <arryan@umich.edu>
2021-07-15 18:59:29 -0400

    create and save mongoose model

---
+++ src/db/mongoose.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+});
+
+//basic User model
+const User = mongoose.model("User", {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+});
+
+// create new user
+const me = new User({
+    name: "Arryan",
+    age: 22
+});
+
+// save the new user
+me.save().then(() => {
+    console.log(me);
+}).catch(error => {
+    console.log("Error", error)
+})
