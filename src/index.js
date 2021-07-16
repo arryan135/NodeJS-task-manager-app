a304260cbf99153feaea76d1785448d49f8d4424
arryan <arryan@umich.edu>
2021-07-16 19:40:09 -0400

    add update endpoint for task

--- src/index.js
+++ src/index.js
@@ -99,6 +99,26 @@ app.get("/tasks/:id", async (req, res) => {
     }
 });
 
+app.patch("/tasks/:id", async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["completed", "description"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if(!isValidOperation){
+        return res.status(400).send({"error": "Invalid Updates"});
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true});
+        if (!task){
+            return res.status(404).send();
+        }
+        res.send(task);
+    } catch(error){
+        res.status(500).send(error);
+    }
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
