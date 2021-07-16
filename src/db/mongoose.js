b411d2c173e98d44b0abd7272b066f7e6fcd79ac
arryan <arryan@umich.edu>
2021-07-16 14:20:00 -0400

    create endpoint for post /tasks

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,21 +1,8 @@
 const mongoose = require("mongoose");
 
-// Connect to the local database
+// Connect mongoose to the local database
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true 
-});
-
-//task model
-const Task = mongoose.model("Task", {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
 });
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 // makes sure that mongoose connexts to the database
-require("./db/mongoose")
-const User = require("./models/user")
+require("./db/mongoose");
+const User = require("./models/user");
+const Task = require("./models/task");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -13,9 +14,19 @@ app.post("/users", (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
     }).catch(error => {
         // set http status & send error
+        res.status(400).send(error);
+    })
+});
+
+app.post("/tasks", (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
+    }).catch(error => {
         res.status(400).send(error);
     })
 });
