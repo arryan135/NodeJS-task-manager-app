a2f31a74c10c14acc7bf5857c83ee3fedd22cd06
arryan <arryan@umich.edu>
2021-07-16 18:08:16 -0400

    explore async await with user collection

--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,13 +1,32 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-User.findByIdAndUpdate("60f1c8caeabcaa873048b83c", {age: 1}).then(user => {
-    console.log(user);
-    return User.countDocuments({age: 1});
-}).then(result => {
-    console.log(result);
+// chaining example
+
+// User.findByIdAndUpdate("60f1c8caeabcaa873048b83c", {age: 1}).then(user => {
+//     console.log(user);
+//     return User.countDocuments({age: 1});
+// }).then(result => {
+//     console.log(result);
+// }).catch(error => {
+//     console.log(error);
+// });
+
+
+// async-await example
+
+const updateAgeAndCount = async (id, age) => {
+    const user = await User.findByIdAndUpdate(id, {age});
+    const count = await User.countDocuments({age});
+    return {user, count};
+}
+
+updateAgeAndCount("60f1c8caeabcaa873048b83c", 18).then(result => {
+    console.log("User updated: ", result.user);
+    console.log("count with age : ", result.count);
 }).catch(error => {
     console.log(error);
-});
+})
+
 
 
