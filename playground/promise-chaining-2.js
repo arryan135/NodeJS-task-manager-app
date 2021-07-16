53459e625f584b99e2d07d04a2b40fccfaa24137
arryan <arryan@umich.edu>
2021-07-16 18:17:17 -0400

    explore async await with task collection

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,6 +1,8 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
+// chaining example
+// Should throw error as this id doesn't exist
 Task.findByIdAndDelete("60f1cc41208ee392f967d083").then(task => {
     console.log(task);
     return Task.countDocuments({completed: false});
@@ -10,3 +12,15 @@ Task.findByIdAndDelete("60f1cc41208ee392f967d083").then(task => {
     console.log(error);
 });
 
+// async await example
+const deleteTaskAndCount = async (id) => {
+    const task = await Task.findByIdAndDelete(id);
+    const count = await Task.countDocuments({completed : false});
+    return count;
+}
+
+deleteTaskAndCount("60f203dc3391c03a4d8c539d").then(count => {
+    console.log("Incomplete tasks :" , count);
+}).catch(error => {
+    console.log(error);
+})
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,20 +1,19 @@
 require("../src/db/mongoose");
 const User = require("../src/models/user");
 
-// chaining example
-
-// User.findByIdAndUpdate("60f1c8caeabcaa873048b83c", {age: 1}).then(user => {
-//     console.log(user);
-//     return User.countDocuments({age: 1});
-// }).then(result => {
-//     console.log(result);
-// }).catch(error => {
-//     console.log(error);
-// });
+// chaining example 
+// Should throw error as this id doesn't exist
+User.findByIdAndUpdate("60f1c8caeabcaa873048b83c", {age: 1}).then(user => {
+    console.log(user);
+    return User.countDocuments({age: 1});
+}).then(result => {
+    console.log(result);
+}).catch(error => {
+    console.log(error);
+});
 
 
 // async-await example
-
 const updateAgeAndCount = async (id, age) => {
     const user = await User.findByIdAndUpdate(id, {age});
     const count = await User.countDocuments({age});
