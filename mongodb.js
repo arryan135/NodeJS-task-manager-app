cb93de87d624e5117c73c456ce32f08e62584246
arryan <arryan@umich.edu>
2021-07-16 13:45:41 -0400

    Change file structure

--- mongodb.js
+++ mongodb.js
@@ -1,36 +1,39 @@
-// MongoClient = gives us access to the function that allows us to perform the CRUD operations
-const {MongoClient, ObjectID} = require("mongodb");
+// THIS FILE IS NO LONGER USED FOR THE TASK MANAGER APPLICATION
 
-const connectionURL = "mongodb://127.0.0.1:27017";
-const databaseName = "task-manager";
+// // MongoClient = gives us access to the function that allows us to perform the CRUD operations
 
-// connect to the local server
-// args: connectionURL, object that parses that URL, async callback function that returns error and client
-// pass useUnifiedTopology to ensure that new server discover and monitor engine is used
-MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
-    if (error) return console.log("Unable to connect to database");
+// const {MongoClient, ObjectID} = require("mongodb");
 
-    const db = client.db(databaseName);
+// const connectionURL = "mongodb://127.0.0.1:27017";
+// const databaseName = "task-manager";
 
-    // delete many
-    // db.collection("users").deleteMany({
-    //     age: 27
-    // }).then(result => {
-    //     console.log(result);
-    // }).catch(error => {
-    //     console.log(error);
-    // });
+// // connect to the local server
+// // args: connectionURL, object that parses that URL, async callback function that returns error and client
+// // pass useUnifiedTopology to ensure that new server discover and monitor engine is used
+// MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+//     if (error) return console.log("Unable to connect to database");
 
-    db.collection("tasks").deleteOne({
-        description: "Feed the cows"
-    }).then(result => {
-        console.log(result);
-    }).catch(error => {
-        console.log(error);
-    })
+//     const db = client.db(databaseName);
+
+//     // delete many
+//     // db.collection("users").deleteMany({
+//     //     age: 27
+//     // }).then(result => {
+//     //     console.log(result);
+//     // }).catch(error => {
+//     //     console.log(error);
+//     // });
+
+//     db.collection("tasks").deleteOne({
+//         description: "Feed the cows"
+//     }).then(result => {
+//         console.log(result);
+//     }).catch(error => {
+//         console.log(error);
+//     })
 
 
-}); 
+// }); 
 
 
 
