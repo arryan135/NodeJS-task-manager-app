eec37c4d043884aaafe851d2f46314680965bfa5
arryan <arryan@umich.edu>
2021-07-16 19:53:13 -0400

    add delete endpoints for task and user

--- src/index.js
+++ src/index.js
@@ -66,6 +66,18 @@ app.patch("/users/:id", async (req, res) => {
     }
 });
 
+app.delete("/users/:id", async (req, res) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user){
+            res.status(404).send();
+        }
+        res.send(user);
+    } catch(error){
+        res.status(500).send();
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     const task = new Task(req.body);
     try {
@@ -116,6 +128,18 @@ app.patch("/tasks/:id", async (req, res) => {
         res.send(task);
     } catch(error){
         res.status(500).send(error);
+    }
+});
+
+app.delete("/tasks/:id", async (req, res) => {
+    try {
+        const task = await Task.findByIdAndDelete(req.params.id);
+        if (!task){
+            res.status(404).send();
+        }
+        res.send(task);
+    } catch (error){
+        res.status(500).send();
     }
 });
 
