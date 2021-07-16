cc3f00cebd1ae5fce9419bcfd4bf4efd9338e5a3
arryan <arryan@umich.edu>
2021-07-16 18:50:28 -0400

    refactor post tasks  endpoint to use async function

--- src/index.js
+++ src/index.js
@@ -44,14 +44,14 @@ app.get("/users", async (req, res) => {
     }
 });
 
-app.post("/tasks", (req, res) => {
+app.post("/tasks", async (req, res) => {
     const task = new Task(req.body);
-
-    task.save().then(() => {
+    try {
+        await task.save(); 
         res.status(201).send(task);
-    }).catch(error => {
+    } catch (error) {
         res.status(400).send(error);
-    })
+    }
 });
 
 app.get("/tasks", (req, res) => {
