5faf87051ada66961349195a7889964293625438
arryan <arryan@umich.edu>
2021-07-16 19:24:29 -0400

    add update endpoint for user

--- src/index.js
+++ src/index.js
@@ -44,6 +44,28 @@ app.get("/users", async (req, res) => {
     }
 });
 
+app.patch("/users/:id", async (req, res) => {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ["name", "email", "password", "age"];
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update));
+
+    if (!isValidOperation){
+        return res.status(400).send({"error": "Invalid Updates"});
+    }
+
+    try {
+        // new:true returns the new user as opposed to the existing one found before the update 
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        // no user with the given id
+        if (!user){
+            return res.status(404).send();
+        }
+        res.send(user);
+    } catch (error){
+        res.status(500).send(error);
+    }
+});
+
 app.post("/tasks", async (req, res) => {
     const task = new Task(req.body);
     try {
