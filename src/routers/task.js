02a565d46d0334410aa5fbaf08183cbdc9bb2634
arryan <arryan@umich.edu>
2021-07-17 22:26:36 -0400

    feat: filter tasks based on query parameters

--- src/routers/task.js
+++ src/routers/task.js
@@ -17,12 +17,21 @@ router.post("/tasks", auth, async (req, res) => {
 });
 
 router.get("/tasks", auth, async (req, res) => {
+    const match = {};
+
+    if (req.query.completed){
+        match.completed = req.query.completed === "true";
+    }
+
     try {
         // normal way
         // const tasks = await Task.find({owner: req.user._id});
 
         // virtual function way
-        await req.user.populate("tasks").execPopulate();
+        await req.user.populate({
+            path: "tasks",
+            match
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch(error){
         res.status(500).send(error);
