07b21de8a824203ed152334e693ba114249f4dea
arryan <arryan@umich.edu>
2021-07-19 00:32:55 -0400

    send welcome email when user created

---
+++ src/emails/account.js
@@ -0,0 +1,20 @@
+const sgMail = require("@sendgrid/mail");
+
+const sendGridAPIKey = "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";
+
+sgMail.setApiKey(sendGridAPIKey);
+
+const sendWelcomeEmail = (email, name) => {
+    sgMail.send({
+        to: email,
+        from: "arryan@umich.edu",
+        subject: "Welcome to Arryan's NodeJS task manager app",
+        text: `Welcome to the app, ${name}. Let me know how you get along with the different routes of this app`
+    })
+}
+
+
+
+module.exports = {
+    sendWelcomeEmail
+}
--- src/routers/user.js
+++ src/routers/user.js
@@ -4,6 +4,8 @@ const User = require("../models/user");
 const router = new express.Router();
 const auth = require("../middleware/auth");
 const multer = require("multer");
+const {sendWelcomeEmail, sendGoodByeEmail} = require("../emails/account");
+
 const upload = multer({
     limits: {
         fileSize: 1000000
@@ -24,6 +26,7 @@ router.post("/users", async (req, res) => {
 
     try {
         await user.save();
+        sendWelcomeEmail(user.email, user.name) // send welcome email
         const token = await user.generateAuthToken();
         res.status(201).send({user, token});
     } catch(error){
