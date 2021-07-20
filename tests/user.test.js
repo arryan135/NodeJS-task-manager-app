3a7e55a746d9f6259a338ca3f06d70142ac800be
arryan <arryan@umich.edu>
2021-07-19 22:37:47 -0400

    add jest test case to check login for existing users

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,10 +1,18 @@
 const request = require("supertest");
 const app = require("../src/app");
-const User = require("../src/models/user")
+const User = require("../src/models/user");
+
+// could be used later for test cases for login, etc
+const userOne = {
+    name: "Arryan Malik",
+    email: "arryanmalik135@gmail.com",
+    password: "thisisthecoolestpass"
+}
 
 // this function run before each test case in this test suite
 beforeEach(async () => {
     await User.deleteMany();
+    await new User(userOne).save();
 });
 
 test("Should sign up a new user", async () => {
@@ -13,5 +21,12 @@ test("Should sign up a new user", async () => {
         email: "arryan@umich.edu",
         password: "thisisagoodpass"
     }).expect(201)
+});
+
+test("Should login existing user", async () => {
+    await request(app).post("/users/login").send({
+        email: userOne.email,
+        password: userOne.password
+    }).expect(200);
 });
 
