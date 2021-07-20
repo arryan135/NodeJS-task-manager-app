b90996b8590b4a753c1f228451631530fced3263
arryan <arryan@umich.edu>
2021-07-19 23:13:09 -0400

    add jest user profile test, with and without authentication

--- tests/user.test.js
+++ tests/user.test.js
@@ -24,7 +24,8 @@ beforeEach(async () => {
 });
 
 test("Should sign up a new user", async () => {
-    await request(app).post("/users")
+    await request(app)
+        .post("/users")
         .send({
             name: "Arryan",
             email: "arryan@umich.edu",
@@ -34,7 +35,8 @@ test("Should sign up a new user", async () => {
 });
 
 test("Should login existing user", async () => {
-    await request(app).post("/users/login")
+    await request(app)
+        .post("/users/login")
         .send({
             email: userOne.email,
             password: userOne.password
@@ -43,10 +45,27 @@ test("Should login existing user", async () => {
 });
 
 test("Should not login non-existant user", async () => {
-    await request(app).post("/users/login")
+    await request(app)
+        .post("/users/login")
         .send({
             email: "test@test.com",
             password: "thisissomecode"
         })
         .expect(400);
+});
+
+test("Should get profile for user", async () => {
+    await request(app)
+        .get("/users/me")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`) // to add Authorization header
+        .send()
+        .expect(200);
+});
+
+test("Should not get profile for unauthenticated user", async () => {
+    await request(app)
+        .get("/users/me")
+        .send()
+        .expect(401);
+        // did not set authentication header so the auth middleware will not know if user is authenticated
 });
