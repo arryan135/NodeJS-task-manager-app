4a7b155b85f892e69b5ca681b0dc6c51a42036db
arryan <arryan@umich.edu>
2021-07-19 22:33:00 -0400

    fix issue: consecutive jest tests now pass as we clear db

--- tests/user.test.js
+++ tests/user.test.js
@@ -1,5 +1,11 @@
 const request = require("supertest");
 const app = require("../src/app");
+const User = require("../src/models/user")
+
+// this function run before each test case in this test suite
+beforeEach(async () => {
+    await User.deleteMany();
+});
 
 test("Should sign up a new user", async () => {
     await request(app).post("/users").send({
