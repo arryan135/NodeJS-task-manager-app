edb75b02b1d66b42b8dd6035bba19fcafbb50627
arryan <arryan@umich.edu>
2021-07-19 20:27:18 -0400

    private config changes

--- src/emails/account.js
+++ src/emails/account.js
@@ -1,8 +1,6 @@
 const sgMail = require("@sendgrid/mail");
 
-const sendGridAPIKey = "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";
-
-sgMail.setApiKey(sendGridAPIKey);
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const sendWelcomeEmail = (email, name) => {
     sgMail.send({
