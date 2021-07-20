c349e1d7ae551a95a3a6313cad811211fe238619
arryan <arryan@umich.edu>
2021-07-20 02:15:48 -0400

    add jest create task for user test

---
+++ tests/task.test.js
@@ -0,0 +1,21 @@
+const request = require("supertest");
+const Task = require("../src/models/task");
+const app = require("../src/app");
+const {userOneId, userOne, setupDatabase} = require("./fixtures/db");
+
+beforeEach(setupDatabase);
+
+test("Should create task for user", async () => {
+    const response = await request(app)
+        .post("/tasks")
+        .set("Authorization", `Bearer ${userOne.tokens[0].token}`)
+        .send({
+            description: "From my jest task tests file"
+        })
+        .expect(201);
+    //check if task is present in db
+    const task = await Task.findById(response.body._id);
+    expect(task).not.toBeNull();
+    expect(task.completed).toEqual(false)
+});
+
--- tests/task.text.js
+++
@@ -1,9 +0,0 @@
-const request = require("supertest");
-const Task = require("../src/models.task");
-const app = require("../src/app");
-const {userOneId, userOne, setupDatabase} = require("./fixtures/db");
-
-beforeEach(setupDatabase);
-
-
-
